c183f760d18c3d64e49cfd202e23cede742aaa42
phuong211zz <phuong211zz@gmail.com>
2021-04-21 17:03:32 +0700

    init

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+    <h1 class="mt-4">hello</h1>
+    <div class="tw-bg-black tw-p-40 tw-text-red-600">Hello</div>
+  </div>
+</template>
+
+<script>
+export default {};
